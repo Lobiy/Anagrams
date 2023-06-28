67f80b3c4c9fe30c2322902dc44a41e5f9f1820a
Yuriy Lobanov <yuriy.lobanov2002@gmail.com>
2023-06-28 16:07:12 +0300

    adding package
    put AnagramMakerTest to right directory
    
    Added comments from merge request
    
    Added comments from merge request
    
    Removed gitignore and idea
    
    Reworked the task:
    1. Splitted application into 2 classes
    2. Added logic for Anagram maker
    3. Worked on validation, added 2 more cases
    
    squash! Readded JaCoCo dependency as it has now 95% of line & branch coverage
    
    Updated jacoco version, so it generates proper report with 95% coverage
    
    Added test #15 to test inner cycle
    
    Added jacoco dependencies
    Removed static from all methods
    
    Renamed 14 Junit tests
    Added property for pom.xml jupiter version
    
    Added 14 Junit 15 tests,
    Found and reworked a problem in reverseWord method,
    Added SonarLint and put into work its suggestions.
    
    Readded JaCoCo dependency as it has now 95% of line & branch coverage
    
    Updated jacoco version, so it generates proper report with 95% coverage
    
    Added test #15 to test inner cycle
    
    Only 2 versions are left
    
    Initial commit
    
    Added jacoco dependencies
    Removed static from all methods
    
    Renamed 14 Junit tests
    Added property for pom.xml jupiter version
    
    Added 14 Junit 15 tests,
    Found and reworked a problem in reverseWord method,
    Added SonarLint and put into work its suggestions.
    
    - Renamed array to symbols
    - added line after last brace
    - reworked pom.xml
    
    Added package anagram to the path
    
    Added package anagram to the path
    
    Removed extra line;
    Added extra line;
    Reworked concat method to String.join();
    Tried to remove commit file
    
    Removed gitignore and idea
    
    Reworked the task:
    1. Splitted application into 2 classes
    2. Added logic for Anagram maker
    3. Worked on validation, added 2 more cases
    
    Reworked the task:
    1. Splitted application into 2 classes
    2. Added logic for Anagram maker
    3. Worked on validation, added 2 more cases
    
    Created reverseLine method, finished the task.
    
    Added package anagram to the path
    
    Added package anagram to the path
    
    Removed extra line;
    Added extra line;
    Reworked concat method to String.join();
    Tried to remove commit file
    
    - Renamed array to symbols
    - added line after last brace
    - reworked pom.xml
    
    Added package anagram to the path
    
    Added package anagram to the path
    
    Removed extra line;
    Added extra line;
    Reworked concat method to String.join();
    Tried to remove commit file
    
    Removed gitignore and idea
    
    Reworked the task:
    1. Splitted application into 2 classes
    2. Added logic for Anagram maker
    3. Worked on validation, added 2 more cases
    
    Reworked the task:
    1. Splitted application into 2 classes
    2. Added logic for Anagram maker
    3. Worked on validation, added 2 more cases
    
    Created reverseLine method, finished the task.
    
    Added package anagram to the path
    
    Added package anagram to the path
    
    Removed extra line;
    Added extra line;
    Reworked concat method to String.join();
    Tried to remove commit file
    
    Reworked the task:
    1. Splitted application into 2 classes
    2. Added logic for Anagram maker
    3. Worked on validation, added 2 more cases
    
    Created reverseLine method, finished the task.
    
    Created reverseLine method, finished the task.
    
    Created reverseLine method, finished the task.
    
    Created reverseSingleWord method
    
    Created reverseSingleWord method

--- src/main/java/org/foxminded/yLobanov/anagram/AnagramMaker.java
+++ src/main/java/org/foxminded/yLobanov/anagram/AnagramMaker.java
@@ -6,7 +6,7 @@ public class AnagramMaker {
 
     private static final String SPACE_DELIMITER = " ";
 
-    public static String makeAnagram(String sentence) {
+    public String makeAnagram(String sentence) {
         validateSentence(sentence);
 
         String[] words = sentence.split(SPACE_DELIMITER);
@@ -18,34 +18,35 @@ public class AnagramMaker {
         return concat(reversedWords);
     }
 
-    private static void validateSentence(String sentence) {
+    private void validateSentence(String sentence) {
         if (sentence == null) {
             throw new IllegalArgumentException("sentence is null");
+        }
+
+        if (sentence.isEmpty()) {
+            throw new IllegalArgumentException("sentence is empty");
         }
 
         if (sentence.matches("\\s*")) {
             throw new IllegalArgumentException("sentence contains only spaces and/or tabs");
         }
 
-        if (sentence.isEmpty()) {
-            throw new IllegalArgumentException("sentence is empty");
-        }
     }
 
-    private static String reversedWord(String word) {
+    private String reversedWord(String word) {
         char[] symbols = new char[word.length()];
         Arrays.fill(symbols, ' ');
-        for(int i = 0; i < word.length(); i++) {
+        for (int i = 0; i < word.length(); i++) {
             char symbol = word.charAt(i);
             if (!Character.isLetter(symbol)) {
                 symbols[i] = symbol;
             }
         }
-        for(int i = 0; i < word.length(); i++) {
+        for (int i = 0; i < word.length(); i++) {
             char symbol = word.charAt(i);
             if (Character.isLetter(symbol)) {
-                int j = word.length() - 1 - i;
-                while(j > -1 && symbols[j] != ' ') {
+                int j = word.length() - 1;
+                while (j > -1 && symbols[j] != ' ') {
                     j--;
                 }
                 symbols[j] = symbol;
@@ -54,7 +55,8 @@ public class AnagramMaker {
         return new String(symbols);
     }
 
-    private static String concat(String[] words) {
+    private String concat(String[] words) {
         return String.join(SPACE_DELIMITER, words).trim();
     }
+
 }
